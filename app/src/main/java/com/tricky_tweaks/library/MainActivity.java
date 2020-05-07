5c552100857452253321771028d460b41e108b7e
pratikKataria <pratikkatariya3.pk@gmail.com>
2020-05-08 02:20:43 +0530

    MainActivity - layout animation create working

--- app/src/main/java/com/tricky_tweaks/library/MainActivity.java
+++ app/src/main/java/com/tricky_tweaks/library/MainActivity.java
@@ -1,8 +1,16 @@
 package com.tricky_tweaks.library;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
 
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.activity_main);
+
+        ConstraintSet constraintSet = new ConstraintSet();
+
+        Transition transition = new ChangeBounds();
+        transition.setInterpolator(new AccelerateDecelerateInterpolator());
+        transition.setDuration(2000);
+
+        constraintLayout.post(() -> {
+            constraintSet.clone(this, R.layout.start_screen_scene_one);
+            TransitionManager.beginDelayedTransition(constraintLayout, transition);
+            constraintSet.applyTo(constraintLayout);
+        });
+
+
     }
 }
