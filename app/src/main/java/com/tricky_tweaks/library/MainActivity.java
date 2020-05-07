3975961186568bbc258b8ed7cf0475cc2e893030
pratikKataria <pratikkatariya3.pk@gmail.com>
2020-05-08 04:42:09 +0530

    MainActivity - Animation completed

--- app/src/main/java/com/tricky_tweaks/library/MainActivity.java
+++ app/src/main/java/com/tricky_tweaks/library/MainActivity.java
@@ -1,38 +1,76 @@
 package com.tricky_tweaks.library;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
-import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
-
 import android.os.Bundle;
+import android.os.Handler;
 import android.transition.ChangeBounds;
 import android.transition.Transition;
 import android.transition.TransitionManager;
+import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.databinding.DataBindingUtil;
+
+import com.tricky_tweaks.library.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isLoginShown = false;
+    private ConstraintSet constraintSetGone = new ConstraintSet();
+    private ConstraintSet constraintSetVisible = new ConstraintSet();
+    private ConstraintLayout constraintLayout;
+    private Transition transition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        ConstraintLayout constraintLayout = findViewById(R.id.activity_main);
 
-        ConstraintSet constraintSet = new ConstraintSet();
-
-        Transition transition = new ChangeBounds();
+        constraintLayout = findViewById(R.id.activity_main);
+        transition = new ChangeBounds();
         transition.setInterpolator(new AccelerateDecelerateInterpolator());
-        transition.setDuration(2000);
+        transition.setDuration(1500);
+
+        constraintSetGone.clone(constraintLayout);
+        constraintSetVisible.clone(this, R.layout.start_screen_scene_one);
 
         constraintLayout.post(() -> {
-            constraintSet.clone(this, R.layout.start_screen_scene_one);
             TransitionManager.beginDelayedTransition(constraintLayout, transition);
-            constraintSet.applyTo(constraintLayout);
+            constraintSetVisible.applyTo(constraintLayout);
         });
 
+        constraintLayout.setOnClickListener(n -> {
+            if (!isLoginShown) {
+                Toast.makeText(this, "clciked", Toast.LENGTH_SHORT).show();
+                TransitionManager.beginDelayedTransition(constraintLayout, transition);
+                constraintSetGone.applyTo(constraintLayout);
+                isLoginShown = true;
+
+                new Handler().postDelayed(() -> {
+                    Animation animation = AnimationUtils.loadAnimation(this, R.anim.btn_pop_anim);
+                    binding.loginIvLoginImg.setVisibility(View.VISIBLE);
+                    binding.loginEtEmail.setVisibility(View.VISIBLE);
+                    binding.loginEtPass.setVisibility(View.VISIBLE);
+                    binding.mbLogin.setVisibility(View.VISIBLE);
+                    binding.loginForgetPassword.setVisibility(View.VISIBLE);
+
+                    binding.mbLogin.setAnimation(animation);
+                    binding.loginMbCreateAccount.setVisibility(View.VISIBLE);
+                    binding.loginMbCreateAccount.setAnimation(animation);
+                    animation.start();
+
+                    binding.imageView11.animate().translationY(200).setDuration(500).setInterpolator(new LinearInterpolator()).start();
+                    binding.imageView12.animate().translationY(300).setDuration(500).setInterpolator(new LinearInterpolator()).start();
+                }, 800);
+            }
+        });
 
     }
 }
