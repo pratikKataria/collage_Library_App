cada4640aa8ffde328b97bee14eea337253e227b
pratikKataria <pratikkatariya3.pk@gmail.com>
2020-05-12 23:53:24 +0530

    App Name Changes and Splash screen layout changed

--- app/src/main/java/com/tricky_tweaks/library/splash/SplashActivity.java
+++ app/src/main/java/com/tricky_tweaks/library/splash/SplashActivity.java
@@ -1,16 +1,45 @@
 package com.tricky_tweaks.library.splash;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.tricky_tweaks.library.MainActivity;
 import com.tricky_tweaks.library.R;
+import com.tricky_tweaks.library.databinding.ActivitySplashBinding;
+import com.tricky_tweaks.library.profile.ProfileActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        ActivitySplashBinding activitySplashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+
+        SplashViewModel splashViewModel = new ViewModelProvider(this).get(SplashViewModel.class);
+        splashViewModel.checkIfUserIsAuthenticated();
+
+        splashViewModel.getIsStudentAuthenticatedLiveData().observe(this, student -> {
+            if (student.isAuthenticated) {
+                splashViewModel.checkIfEnrollmentNumberPresent();
+            }
+        });
+
+        splashViewModel.getIsEnrollmentNumberExist().observe(this, aBoolean -> {
+            if (aBoolean) {
+                start(MainActivity.class);
+            } else {
+                start(ProfileActivity.class);
+            }
+        });
+    }
+
+    void start(Class<? extends AppCompatActivity> token) {
+        Intent intent = new Intent(SplashActivity.this, token);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/tricky_tweaks/library/splash/SplashViewModel.java
+++ app/src/main/java/com/tricky_tweaks/library/splash/SplashViewModel.java
@@ -1,4 +1,33 @@
 package com.tricky_tweaks.library.splash;
 
-public class SplashViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.tricky_tweaks.library.model.Student;
+
+public class SplashViewModel extends ViewModel {
+    private SplashRepository splashRepository;
+    private MutableLiveData<Boolean> isEnrollmentNumberExist = new MutableLiveData<>();
+    private MutableLiveData<Student> isStudentAuthenticatedLiveData = new MutableLiveData<>();
+
+    public SplashViewModel() {
+        splashRepository = new SplashRepository();
+    }
+
+    void checkIfUserIsAuthenticated() {
+        isStudentAuthenticatedLiveData = splashRepository.checkIfUserIsAuthenticatedInFirebase();
+    }
+
+    void checkIfEnrollmentNumberPresent() {
+        isEnrollmentNumberExist = splashRepository.checkEnrollmentNumberInFirebaseStore(isStudentAuthenticatedLiveData.getValue().getUid());
+    }
+
+    LiveData<Boolean> getIsEnrollmentNumberExist() {
+        return isEnrollmentNumberExist;
+    }
+
+    LiveData<Student> getIsStudentAuthenticatedLiveData() {
+        return isStudentAuthenticatedLiveData;
+    }
 }
