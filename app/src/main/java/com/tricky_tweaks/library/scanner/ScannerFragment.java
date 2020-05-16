023d44486d3e6b3a48ecbcef21c2c1ab57717667
pratikKataria <pratikkatariya3.pk@gmail.com>
2020-05-16 23:22:10 +0530

    Scanner module -> ScannerFragment - bug fixes like rescan and exit code

--- app/src/main/java/com/tricky_tweaks/library/scanner/ScannerFragment.java
+++ app/src/main/java/com/tricky_tweaks/library/scanner/ScannerFragment.java
@@ -1,5 +1,7 @@
 package com.tricky_tweaks.library.scanner;
 
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +19,10 @@ import com.tricky_tweaks.library.R;
 import com.tricky_tweaks.library.databinding.FragmentScannerBinding;
 import com.tricky_tweaks.library.utils.Constants;
 import com.tricky_tweaks.library.utils.FragmentQRCodeReader;
+
+import static com.tricky_tweaks.library.utils.Constants.IConstants.APP_CONFIG;
+import static com.tricky_tweaks.library.utils.Constants.IQRCode.ENTER;
+import static com.tricky_tweaks.library.utils.Constants.IQRCode.EXIT;
 
 /**
  * created by pratik katariya
@@ -69,7 +75,7 @@ public class ScannerFragment extends FragmentQRCodeReader {
     private void initScanner() {
         /*
          * since barcodeReaderFragment is the child fragment in the ScannerFragment therefore
-         * it is get using getChildfFragmentManager
+         * it is get using getChildFragmentManager
          * setting up the custom bar code listener
          */
         barcodeReaderFragment = (BarcodeReaderFragment) getChildFragmentManager().findFragmentById(R.id.scannerFragment);
@@ -95,23 +101,73 @@ public class ScannerFragment extends FragmentQRCodeReader {
     public void onScanned(Barcode barcode) {
         super.onScanned(barcode);
 
-        isScanning.set(false);
-
         //on Scan completed close the scanner
-        if (!isScanning.get()) {
+        if (isScanning.get()) {
             getChildFragmentManager()
                     .beginTransaction()
                     .remove(getChildFragmentManager().findFragmentById(R.id.scannerFragment))
                     .commit();
         }
 
+        isScanning.set(false);
+
+        /*
+        * this works when if user scan the exist
+        * code without scanning the enter code
+        */
+        boolean waitingForEnterCode = !getUpdatedPendingEntryEvent();
+        if (barcode.rawValue.equals(EXIT) && waitingForEnterCode) {
+            String message = "Its look like that you haven't scan the Enter Code " + new String(Character.toChars(0x1F622));
+            showAlertDialog(message);
+            return;
+        }
+
+        /*
+        * this works when user scan the enter code again
+        * without scanning the exit code
+        */
+        boolean waitingForExitCode = getUpdatedPendingEntryEvent();
+        if (barcode.rawValue.equals(ENTER) && waitingForExitCode) {
+            String message = "you have already scan the Enter code First scan a Exist " + new String(Character.toChars(0x1F622));
+            showAlertDialog(message);
+            return;
+        }
+
         if (barcode.rawValue.equals(Constants.IQRCode.ENTER)) {
             viewmodel.updateScannedValueWhenEnter();
-        } else if (barcode.rawValue.equals(Constants.IQRCode.EXIT)) {
+            updatePendingEntryEvent(true);
+        } else if (barcode.rawValue.equals(EXIT)) {
             viewmodel.updateScannedValueWhenExist();
+            updatePendingEntryEvent(false);
         } else {
             Toast.makeText(getActivity(), "no action specified", Toast.LENGTH_SHORT).show();
         }
-        
+    }
+
+    /**
+     * custom message you want to show to the user
+     * @param message
+     */
+    private void showAlertDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setTitle("QR Scanner")
+                .setMessage(message)
+                .setPositiveButton("OKAY", (dialog, which) -> {
+                    dialog.dismiss();
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    void updatePendingEntryEvent(boolean isEntryPending) {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(APP_CONFIG, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("ENTRY_EVENT", isEntryPending);
+        editor.apply();
+    }
+
+    boolean getUpdatedPendingEntryEvent(){
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(APP_CONFIG, 0);
+        return sharedPreferences.getBoolean("ENTRY_EVENT", false);
     }
 }
